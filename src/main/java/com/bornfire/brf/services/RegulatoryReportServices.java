2199d6821e9b29d71e5c4050383f6d6dd323c990
HARIHARAN M V <harikvg031207@gmail.com>
2025-10-19 01:04:52 +0530

    Update RegulatoryReportServices.java

--- src/main/java/com/bornfire/brf/services/RegulatoryReportServices.java
+++ src/main/java/com/bornfire/brf/services/RegulatoryReportServices.java
@@ -214,6 +214,9 @@ public class RegulatoryReportServices {
 	@Autowired
 	BRRS_M_SECL_ReportService brrs_m_secl_reportservice;
 	
+	@Autowired
+	BRRS_M_SRWA_12G_ReportService brrs_m_srwa_12g_reportservice;
+	
 
 	private static final Logger logger = LoggerFactory.getLogger(RegulatoryReportServices.class);
 
@@ -525,6 +528,12 @@ public class RegulatoryReportServices {
 		case "M_SECL":
 			repsummary = brrs_m_secl_reportservice.getM_SECLView(reportId, fromdate, todate, currency, dtltype, pageable,type, version);
 			break;
+			
+		case "M_SRWA_12G":
+			repsummary = brrs_m_srwa_12g_reportservice.getM_SRWA_12GView(reportId, fromdate, todate, currency, dtltype, pageable,type, version);
+			break;
+			
+			
 		}
 		return repsummary;
 	}
@@ -1373,7 +1382,15 @@ public class RegulatoryReportServices {
 			}
 			break;
 					
-					
+
+		case "M_SRWA_12G":
+			try {
+				repfile = brrs_m_srwa_12g_reportservice.getM_SRWA_12GExcel(filename, reportId, fromdate, todate, currency, dtltype,type,version);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;					
 					
 					
 
@@ -1940,6 +1957,19 @@ public class RegulatoryReportServices {
 				e.printStackTrace();
 			}
 			break;
+			
+			
+		case "M_SRWA_12G":
+			try {
+				archivalData = brrs_m_srwa_12g_reportservice.getM_SRWA_12GArchival();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+			
+			
+			
 		}
 		return archivalData;
 	}
