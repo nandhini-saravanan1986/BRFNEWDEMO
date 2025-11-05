331deedcc98cb87a5c411258114e59e53deac620
Rishinath <arishinath24@gmail.com>
2025-11-05 13:21:12 +0530

    M_BOP Report Files.
    Commiting M_BOP static Report Files.

--- src/main/java/com/bornfire/brf/entities/BRRS_M_BOP_Archival_Summary_Repo.java
+++ src/main/java/com/bornfire/brf/entities/BRRS_M_BOP_Archival_Summary_Repo.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+
+
 public interface BRRS_M_BOP_Archival_Summary_Repo extends JpaRepository<M_BOP_Archival_Summary_Entity, Date>{
 	
 	@Query(value = "select REPORT_DATE, REPORT_VERSION from BRRS_M_BOP_ARCHIVALTABLE_SUMMARY order by REPORT_VERSION", nativeQuery = true)
